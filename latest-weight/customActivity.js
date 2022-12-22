76b8d9b5f3bc9f856abe21c88bf807534f3cb6ac
Adriano Melo <adriano.melo@oviva.com>
2022-12-22 20:49:15 +0100

    test

--- latest-weight/customActivity.js
+++ latest-weight/customActivity.js
@@ -204,6 +204,7 @@ define(["postmonger"], function (Postmonger) {
         progress2: '{{OEvent.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:Progress__c}}',
         personContactId: '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
         '7df0a6ea-c1f5-48cf-8a6d-5b9c68b6484a': '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
+        anotherTest: '{{Interaction.REST-4.value_decimal}}',
         parameters: {
           patientUserId: 'ddabbb91-f256-4748-9d69-453f82ccc942',
           daysSince: 12,
@@ -211,7 +212,7 @@ define(["postmonger"], function (Postmonger) {
           personContactId: '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
           progress: '{{OEvent.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:Progress__c}}'
         },
-        schema: schema.map(x => ({...x, test: "{{" + x.key + "}}"}))
+        schema
     }];
 
     payload["metaData"].isConfigured = true;
