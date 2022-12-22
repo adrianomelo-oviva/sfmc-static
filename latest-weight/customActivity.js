e65293564ec8b94a6deb925f9554f202007c6466
Adriano Melo <adriano.melo@oviva.com>
2022-12-22 20:53:40 +0100

    remove anothertest

--- latest-weight/customActivity.js
+++ latest-weight/customActivity.js
@@ -200,13 +200,9 @@ define(["postmonger"], function (Postmonger) {
 
     payload["arguments"].execute.inArguments = [{
         query_id: '7df0a6ea-c1f5-48cf-8a6d-5b9c68b6484a',
-        progress: '{{OCS_PathwayState__c:Progress__c}}',
-        progress2: '{{OEvent.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:Progress__c}}',
-        personContactId: '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
-        '7df0a6ea-c1f5-48cf-8a6d-5b9c68b6484a': '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
-        anotherTest: '{{Interaction.REST-4.value_decimal}}',
         parameters: {
           patientUserId: 'ddabbb91-f256-4748-9d69-453f82ccc942',
+          '7df0a6ea-c1f5-48cf-8a6d-5b9c68b6484a': '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
           daysSince: 12,
           daysUntil: 2,
           personContactId: '{{Event.DEAudience-a145fcce-6a7e-127d-04a9-4c2aa6c46242.OCS_PathwayState__c:PatientUUID__c}}',
